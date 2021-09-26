404ef6ec6184ec28c6c3c5c6f1bf28c9e4b4903a
lvivduncan <lvivduncan@gmail.com>
2021-09-26 23:24:56 +0300

    first result

--- js/init.js
+++ js/init.js
@@ -1,33 +1,32 @@
 // 26-09-2021
 
+const body = document.getElementsByTagName('body')[0];
+
+// all goods
+const blocks = document.querySelectorAll('.block');
+
+// all buttons goods
 const buttons = document.querySelectorAll('.button');
 const length = buttons.length;
 
-const orderDiv = document.getElementById('order');
+// view block
+const orderDiv = document.querySelector('.CommentFormText textarea');
 
+// basket
 const checkout = document.getElementById('checkout');
+
+// goods in basket
+const checkoutGoods = document.getElementById('checkout-goods');
 
 for(let i = 0; i < length; i++ ){
 
     buttons[i].addEventListener('click', function() {
 
-        // change button name
-        this.innerHTML = 'Замовлено';
-
-        // view checkout
-        checkout.className = 'active';
-
-        // return defautl button name
-        setTimeout(() => {
-
-            this.innerHTML = 'Замовити';
-        }, 1000);
-
         if(localStorage.getItem('order') !== null){
             
             // if localStorage data if exists
             let dataLocalStorage = localStorage.getItem('order');
-            dataLocalStorage += buttons[i].dataset.content + '/n';
+            dataLocalStorage += buttons[i].dataset.content + '\n';
             localStorage.setItem('order', dataLocalStorage);
         } else {
 
@@ -36,24 +35,48 @@ for(let i = 0; i < length; i++ ){
         }
 
         // lighting current
-        const current = this;
+        const current = this.parentNode;
 
         for(let k = 0; k < length; k++){
 
-            buttons[k].classList.add('active');
+            blocks[k].classList.add('active');
             current.classList.remove('active');
+            body.classList.add('active');
         }
 
+        // change button name
+        this.innerHTML = 'Замовлено';
+
+        // view checkout
+        checkout.className = 'active';
+
+        setTimeout(() => {
+            
+            // return defautl button name
+            this.innerHTML = 'Замовити';
+
+            for(let k = 0; k < length; k++){
+
+                blocks[k].classList.remove('active');
+                body.classList.remove('active');
+            }
+        }, 2500);
+
+        // upgrade view checkout
+        checkout && (checkoutGoods.innerHTML = localStorage.getItem('order').replace(/\n/g, "<hr>"));
     });
 }
 
 if(localStorage.getItem('order') !== null){
 
     // view checkout block
-    checkout.className = 'active';
+    checkout && (checkout.className = 'active');
+
+    checkout && (checkoutGoods.innerHTML = localStorage.getItem('order').replace(/\n/g, "<hr>"));
 
     // view localStorage data in basket
     let dataLocalStorage = localStorage.order;
-    orderDiv && (orderDiv.innerHTML = dataLocalStorage);
+    // orderDiv && (orderDiv.innerHTML = dataLocalStorage);
+    orderDiv && (orderDiv.value = dataLocalStorage);
 }
 
